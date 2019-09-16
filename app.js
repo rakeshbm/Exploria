b9983b792d74197e546f8d3d2b2bb6ab0464e02a
Rakesh Bhimappa Mali <rmali@usc.edu>
2019-09-16 05:57:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,66 +6,31 @@ var url = require('url');
 var EventEmitter = require("events").EventEmitter;
 var request = require('request');
 'use strict';
-const helmet = require('helmet');
-const csp = require('helmet-csp');
-var session = require('express-session');
-var validator = require('express-validator');
-require('dotenv').config();
 
-app.set('trust proxy', 1);
+
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(validator());
-app.use(function(req, res, next) {
-  for (var item in req.body) {
-    req.sanitize(item).escape();
-  }
-  next();
-});
-app.use(helmet.hsts({
-  maxAge: 7776000000,
-  includeSubdomains: true
-}));
-app.use(csp({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ['*.google-analytics.com'],
-    styleSrc: ["'unsafe-inline'"],
-    imgSrc: ['*.google-analytics.com'],
-    connectSrc: ["'none'"],
-    fontSrc: ["'self'"],
-    objectSrc: ["'none'"]
-  }
-}));
-app.use(session({
-  secret: 'hidden sess',
-  cookie: {
-    maxAge: 3600000,
-    secure: true,
-    httpOnly: true
-  }
-}));
 
 app.get('/next', (req, res) => {
   res.setHeader('Content-Type', "application/json");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  var nextPage = new EventEmitter();
+  var bd = new EventEmitter();
   var token = req.query.token;
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken="+token+"&key="+process.env.GOOGLE_API_KEY;
-  https.get(url, res => {
-    res.setEncoding("utf8");
+  var url3 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken="+token+"&key=AIzaSyAiR9M-WgtlwdnVInIPg6KBs96qoFf1tS4";
+  https.get(url3, res3 => {
+    res3.setEncoding("utf8");
     var html = "";
     var tmp = {};
-    res.on("data", data => {
+    res3.on("data", data => {
       html += data;
     });
-    res.on("end", () => {
+    res3.on("end", () => {
       html = JSON.parse(html);
-      var results = html.results;
-      var length = results.length;
+      var resu = html.results;
+      var length = resu.length;
       tmp['places'] = [];
       tmp['token'] = "";
       if (html.next_page_token != 'undefined') {
@@ -79,33 +44,33 @@ app.get('/next', (req, res) => {
         tmp.places[i]['place_id'] = resu[i]['place_id'];
         tmp.places[i]['location'] = resu[i]['geometry']['location'];
       }
-      nextPage.result = tmp;
-      nextPage.emit('update');
+      bd.result = tmp;
+      bd.emit('update');
     })
   })
-  nextPage.on('update', function() {
-    res.send(nextPage.result);
-  });
-});
+  bd.on('update', function() {
+    res.send(bd.result);
+  })
+})
 
 app.get('/yelp', (req, res) => {
   res.setHeader('Content-Type', "application/json");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  var yelpPage = new EventEmitter();
-  var query = req.query;
-  var name = query.name;
-  var addr1 = query.address1;
-  var addr2 = query.address2;
-  var city = query.city;
-  var state = query.state;
-  var country = query.country;
+  var bd = new EventEmitter();
+  var que = req.query;
+  var name = que.name;
+  var addr1 = que.address1;
+  var addr2 = que.address2;
+  var city = que.city;
+  var state = que.state;
+  var country = que.country;
   var headers = {
-    'Access-Control-Allow-Origin': 'http://localhost:8081/',
-    'Authorization': 'Bearer '+ process.env.YELP_KEY
+    // 'Access-Control-Allow-Origin': 'http://localhost:8081/',
+    'Authorization': 'Bearer Qy-YxhQ8MycJw68-woih7f5YG9CuCiWpyQsau32mLiYECsh3eLM5cXb_TzMFrDbKRrPZl2abzrq2D51tsKUOiifA6XGontfXj2zj-DHjIabW2m8fYLE_EOmOyEmZXHYx'
   };
 
-  var apiKey = process.env.YELP_API_KEY;
+  var apiKey = "Qy-YxhQ8MycJw68-woih7f5YG9CuCiWpyQsau32mLiYECsh3eLM5cXb_TzMFrDbKRrPZl2abzrq2D51tsKUOiifA6XGontfXj2zj-DHjIabW2m8fYLE_EOmOyEmZXHYx";
   const yelp = require('yelp-fusion');
 
   const client = yelp.client(apiKey);
@@ -117,6 +82,7 @@ app.get('/yelp', (req, res) => {
     state: state,
     country: country
   }).then(response => {
+    console.log(response);
     if (response.jsonBody.businesses.length != 0) {
       yelpReviews(response.jsonBody.businesses[0].id);
     }
@@ -126,34 +92,34 @@ app.get('/yelp', (req, res) => {
   }).catch(e => {
     res.send("error");
   });
-
   function yelpReviews(id) {
-    const yelp = require('yelp-fusion');
-    const clientReviews = yelp.client(apiKey);
-    clientReviews.reviews(id).then(response => {
-    yelpPage.result = response.jsonBody.reviews;
-    yelpPage.emit('update');
+    const yelpR = require('yelp-fusion');
+    const clientR = yelpR.client(apiKey);
+    clientR.reviews(id).then(responseR => {
+    bd.result = responseR.jsonBody.reviews;
+    console.log(bd.result);
+    bd.emit('update');
     }).catch(e => {
       res.send(JSON.stringify("error"));
     });
   }
-  yelpPage.on('update', function() {
-    res.send(yelpPage.result);
-  });
+  bd.on('update', function() {
+    res.send(bd.result);
+  })
 })
 
 app.get('/messages', (req, res) => {
   res.setHeader('Content-Type', "application/json");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  var query = req.query;
-  var keyword = query.keyword;
-  var category = query.category.toLowerCase();
-  var distance = (query.distance == 'undefined') ? 16093.44 : query.distance*1609.344;
-  var location = query.location;
-  var lon = parseFloat(query.lon);
-  var lat = parseFloat(query.lat);
-  var searchPage = new EventEmitter();
+  var que = req.query;
+  var keyword = que.keyword;
+  var category = que.category.toLowerCase();
+  var distance = (que.distance == 'undefined') ? 16093.44 : que.distance*1609.344;
+  var location = que.location;
+  var lon = que.lon;
+  var lat = que.lat;
+  var bd = new EventEmitter();
   if (location != "undefined") {
     unde(distance, category, keyword);
   }
@@ -162,41 +128,41 @@ app.get('/messages', (req, res) => {
   }
 
   function de(lat, lon, distance, category, keyword) {
-    var url2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lon+"&radius="+distance+"&type="+category+"&keyword="+keyword+"&key="+process.env.GOOGLE_API_KEY;
+    var url2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lon+"&radius="+distance+"&type="+category+"&keyword="+keyword+"&key=AIzaSyAiR9M-WgtlwdnVInIPg6KBs96qoFf1tS4";
     https.get(url2, res2 => {
       res2.setEncoding("utf8");
-      var tmp = "";
+      var rr = "";
       res2.on("data", data => {
-        tmp += data;
+        rr += data;
       });
       res2.on("end", () => {
-        tmp = JSON.parse(tmp);
-        var results = tmp.results;
-        var length = results.length;
+        rr = JSON.parse(rr);
+        var resu = rr.results;
+        var length = resu.length;
         var result = [];
         result = {};
         result['token'] = "";
         result['places'] = [];
 
-        if (tmp.next_page_token != 'undefined') {
-          result['token'] = tmp.next_page_token;
+        if (rr.next_page_token != 'undefined') {
+          result['token'] = rr.next_page_token;
         }
         for (var i = 0; i < length; i++) {
           result.places[i] = {};
-          result.places[i]['icon'] = results[i]['icon'];
-          result.places[i]['name'] = results[i]['name'];
-          result.places[i]['vicinity'] = results[i]['vicinity'];
-          result.places[i]['place_id'] = results[i]['place_id'];
-          result.places[i]['location'] = results[i]['geometry']['location'];
+          result.places[i]['icon'] = resu[i]['icon'];
+          result.places[i]['name'] = resu[i]['name'];
+          result.places[i]['vicinity'] = resu[i]['vicinity'];
+          result.places[i]['place_id'] = resu[i]['place_id'];
+          result.places[i]['location'] = resu[i]['geometry']['location'];
         }
-        searchPage.result = result;
-        searchPage.emit('update');
+        bd.result = result;
+        bd.emit('update');
       })
     })
   }
 
   function unde(distance, category, keyword) {
-    var url1 = "https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key="+process.env.GOOGLE_API_KEY;
+    var url1 = "https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key=AIzaSyAiR9M-WgtlwdnVInIPg6KBs96qoFf1tS4";
     https.get(url1, res1 => {
       res1.setEncoding("utf8");
       var body = "";
@@ -205,20 +171,20 @@ app.get('/messages', (req, res) => {
       });
       res1.on("end", () => {
         body = JSON.parse(body);
-        var lon = parsefloat(body.results[0].geometry.location.lng);
-        var lat = parseFloat(body.results[0].geometry.location.lat);
+        var lon = body.results[0].geometry.location.lng;
+        var lat = body.results[0].geometry.location.lat;
         de(lat, lon, distance, category, keyword);
-      });
-    });
+      })
+    })
   }
 
-  searchPage.on('update', function() {
-    res.send(searchPage.result);
+  bd.on('update', function() {
+    res.send(bd.result);
   })
 
 })
 
 
-var server = app.listen(8081, () => {
+var server = app.listen(process.env.PORT || 8081, () => {
   console.log('server is listening on port', server.address().port);
 })
